75b3bf3219c7e23108be8e1da009dcd822fefaa0
R00T@LAM1R0 <lamiro3@naver.com>
2020-08-19 23:53:46 +0900

    SEIR로 개선, 크롤링 관련 부분 일부 변경

--- COVID_19_forecasting_model_003.py
+++ COVID_19_forecasting_model_003.py
@@ -10,9 +10,12 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 ######################################## WebDriver #######################################
+options = webdriver.ChromeOptions()
+options.add_argument('headless') #크롬 브라우저 백그라운드로 실행
+options.add_argument('disable-gpu') #gpu가속 disable
 
-SIRdriver_KOR = webdriver.Chrome('COVID_19_forecasting_model\chromedriver.exe')
-POPdriver_KOR = webdriver.Chrome('COVID_19_forecasting_model\chromedriver.exe')
+SIRdriver_KOR = webdriver.Chrome(r'COVID_19_forecasting_model\chromedriver.exe', chrome_options = options)
+POPdriver_KOR = webdriver.Chrome(r'COVID_19_forecasting_model\chromedriver.exe', chrome_options = options)
 
 SIRdriver_KOR.get('http://ncov.mohw.go.kr/')
 POPdriver_KOR.get('https://superkts.com/population/all')
@@ -27,25 +30,25 @@ POPsoup_KOR = BeautifulSoup(POPhtml_KOR, 'html.parser')
 
 ######################################## GRAPH #######################################
 class Graph():
-    def SIR(self, x, t, beta, theta): # t = 시간(일), beta = 감염의 효과율, theta = 회복률 = 잠복기간의 역수
+    def SIR(self, x, t, beta, gamma): # t = 시간(일), beta = 감염의 효과율, gamma = 회복률 = 잠복기간의 역수
         s = x[0] # 전체 취약자수
         i = x[1] # 전체 감염자수
         r = x[2] # 전체 회복자수
 
         dsdt = -beta*s*i #시간에 따른 취약자수 변화율
-        didt = beta*s*i - theta*i #시간에 따른 감염자수 변화율
-        drdt = theta*i #시간에 따른 회복자수 변화율
+        didt = beta*s*i - gamma*i #시간에 따른 감염자수 변화율
+        drdt = gamma*i #시간에 따른 회복자수 변화율
 
         y = [dsdt, didt, drdt]
         return y
     
-    def _plot(self, func, x, t, beta, theta):
-        result = odeint(func, x, t, args=(beta, theta))
-        plt.plot(t, result[:,0], 'b', label='Susceptible') #취약자 동향   
-        plt.plot(t, result[:,1], 'r', label='Infected') #감염자 동향
-        plt.plot(t, result[:,2], 'g', label='Recovered') #회복자 동향
+    def _plot(self, func, x, t, beta, gamma, N):
+        result = odeint(func, x, t, args=(beta, gamma))
+        plt.plot(t, result[:,0]/N*100, 'b', label='Susceptible') #취약자 동향 - 백분율
+        plt.plot(t, result[:,1]/N*100, 'r', label='Infected') #감염자 동향 - 백분율
+        plt.plot(t, result[:,2]/N*100, 'g', label='Recovered') #회복자 동향 - 백분율
         plt.xlabel('days')
-        plt.ylabel('number of people')
+        plt.ylabel('percentage of each population (%)')
         plt.title('Republic of Korea')
         plt.legend(loc="upper right")
 
@@ -69,11 +72,11 @@ class get_KOR_Data():
         return confirmed_data_IR
     
     def getPOP(self, soup):
-        for pre_data_2 in soup.find_all('article', class_='result'):
-            confirmed_data_POP = pre_data_2.find_all('h1')
+        for pre_data_2 in soup.find_all('article', class_='intro'):
+            confirmed_data_POP = pre_data_2.find_all('b')
         confirmed_data_POP[0] = confirmed_data_POP[0].get_text()
-        a = confirmed_data_POP[0]
-        confirmed_data_POP[0] = a[:10]
+        #a = confirmed_data_POP[0]
+        #confirmed_data_POP[0] = a[:10]
         return confirmed_data_POP
     
     def convertToInt(self, _list):
@@ -113,7 +116,7 @@ class MyWindow(QWidget, Graph):
         self.initUI()
 
     def initUI(self):
-        self.fig = plt.figure(figsize=(10,10)) #출력되는 그래프 크기
+        self.fig = plt.figure(figsize=(70,70)) #출력되는 그래프 크기
         self.canvas = FigureCanvas(self.fig)
         self.toolbar = NavigationToolbar(self.canvas, self)
 
@@ -125,7 +128,7 @@ class MyWindow(QWidget, Graph):
 
         self.setLayout(layout)
 
-        self.setGeometry(300, 300, 300, 300)
+        self.setGeometry(300, 300, 700, 500)
         self.setWindowTitle("COVID-19 SIR forecasting model")
 
         self.show()
@@ -134,24 +137,26 @@ printGraph = Graph()
 GKD = get_KOR_Data(list(), list())
 
 pre_POP_KOR = GKD.convertToInt(GKD.getPOP(POPsoup_KOR)) #인구
-pre_IR_KOR = GKD.convertToInt(GKD.getIR(SIRsoup_KOR)) #(순서대로) - [0]누적 확진자수, [1]회복자수, [2]격리자수(치료자수), [3]사망자수
+pre_IR_KOR = GKD.convertToInt(GKD.getIR(SIRsoup_KOR)) #(순서대로) - [0]누적 확진자수, [1]회복자수(완치자수), [2]격리자수(치료자수), [3]사망자수
 
 # <quit all drivers>
-SIRdriver_KOR.quit() 
+SIRdriver_KOR.quit()
 POPdriver_KOR.quit()
 
 # <constant>
-covid_19_R0 = 5.35 #코로나 바이러스 기초감염재생산수 = 5 ~ 5.7 이지만 임의로 3으로 지정함
-covid_19_R0_deviation = np.arange(5, 5.71, 0.01) #코로나 바이러스 기초감염재생산수 편차 저장용 리스트
-theta_input = 1/14 #코로나 바이러스 잠복기간 = 약 14일 이내 --> 회복률은 잠복기간의 역수인 1/14
+covid_19_R0 = 2.25 #코로나 바이러스 기초감염재생산수
+gamma_input = 1/14 #코로나 바이러스 잠복기간 = 약 14일 이내 --> 회복률은 잠복기간의 역수인 1/14
 
 # <variable - korea>
 Population_input_KOR = pre_POP_KOR[0] #모집단 인구
-beta_input_KOR = theta_input*covid_19_R0/(Population_input_KOR - pre_IR_KOR[0]) # beta = theta*R0/S
-t_input_KOR = np.linspace(0, 100) #시간 범위 설정(가로축 범위 설정)
-x_input_KOR = [Population_input_KOR - pre_IR_KOR[0], pre_IR_KOR[2], pre_IR_KOR[1]] # 차례대로 S(취약자수), I(감염자수), R(회복자수)
+Susceptible = Population_input_KOR - pre_IR_KOR[1] - pre_IR_KOR[2]
+Infected = pre_IR_KOR[2]
+Recovered = pre_IR_KOR[1]
+beta_input_KOR = gamma_input*covid_19_R0/Susceptible # beta = gamma*R0/S
+t_input_KOR = np.linspace(0, 500) #시간 범위 설정(가로축 범위 설정) - 단위: 일(day)
+x_input_KOR = [Susceptible, Infected, Recovered]
 
 app=QApplication(sys.argv)
 w=MyWindow()
-printGraph._plot(printGraph.SIR, x_input_KOR, t_input_KOR, beta_input_KOR, theta_input)
+printGraph._plot(printGraph.SIR, x_input_KOR, t_input_KOR, beta_input_KOR, gamma_input, Population_input_KOR)
 sys.exit(app.exec_()) # exec_()가 루프를 생성하는 함수
--- COVID_19_forecasting_model_004.py
+++ COVID_19_forecasting_model_004.py
@@ -10,14 +10,17 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 ######################################## WebDriver #######################################
+options = webdriver.ChromeOptions()
+options.add_argument('headless') #크롬 브라우저 백그라운드로 실행
+options.add_argument('disable-gpu') #gpu가속 disable
 
-SIRdriver_KOR = webdriver.Chrome('COVID_19_forecasting_model\chromedriver.exe')
-POPdriver_KOR = webdriver.Chrome('COVID_19_forecasting_model\chromedriver.exe')
+SIRdriver_KOR = webdriver.Chrome(r'COVID_19_forecasting_model\chromedriver.exe', chrome_options = options)
+POPdriver_KOR = webdriver.Chrome(r'COVID_19_forecasting_model\chromedriver.exe', chrome_options = options)
 
-SIRdriver_KOR.get('http://ncov.mohw.go.kr/')
+SIRdriver_KOR.get('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=')
 POPdriver_KOR.get('https://superkts.com/population/all')
 
-assert "코로나바이러스감염증-19(COVID-19)" in SIRdriver_KOR.title #title이 다음과 다르다면 오류 뜨게끔 함
+assert "코로나바이러스감염증-19 > 발생동향 > 국내 발생 현황" in SIRdriver_KOR.title #title이 다음과 다르다면 오류 뜨게끔 함
 
 SIRhtml_KOR = SIRdriver_KOR.page_source
 POPhtml_KOR = POPdriver_KOR.page_source
@@ -27,25 +30,28 @@ POPsoup_KOR = BeautifulSoup(POPhtml_KOR, 'html.parser')
 
 ######################################## GRAPH #######################################
 class Graph():
-    def SIR(self, x, t, beta, theta): # t = 시간(일), beta = 감염의 효과율, theta = 회복률 = 잠복기간의 역수
+    def SEIR(self, x, t, beta, gamma, sigma): # t = 시간(일), beta = 감염의 효과율, gamma = 회복률 = 감염지속시간의 역수, sigma = 평균잠복기간의 역수
         s = x[0] # 전체 취약자수
         i = x[1] # 전체 감염자수
         r = x[2] # 전체 회복자수
+        e = x[3] # 전체 잠복자(?)수
 
         dsdt = -beta*s*i #시간에 따른 취약자수 변화율
-        didt = beta*s*i - theta*i #시간에 따른 감염자수 변화율
-        drdt = theta*i #시간에 따른 회복자수 변화율
+        didt = sigma*e - gamma*i #시간에 따른 감염자수 변화율
+        drdt = gamma*i #시간에 따른 회복자수 변화율
+        dedt = beta*s*i - sigma*e #시간에 따른 잠복자수 변화율
 
-        y = [dsdt, didt, drdt]
+        y = [dsdt, didt, drdt, dedt]
         return y
     
-    def _plot(self, func, x, t, beta, theta):
-        result = odeint(func, x, t, args=(beta, theta))
-        plt.plot(t, result[:,0], 'b', label='Susceptible') #취약자 동향   
-        plt.plot(t, result[:,1], 'r', label='Infected') #감염자 동향
-        plt.plot(t, result[:,2], 'g', label='Recovered') #회복자 동향
+    def _plot(self, func, x, t, beta, gamma, sigma, N):
+        result = odeint(func, x, t, args=(beta, gamma, sigma))
+        plt.plot(t, result[:,0]/N*100, 'b', label='Susceptible') #취약자 동향 - 백분율
+        plt.plot(t, result[:,1]/N*100, 'r', label='Infected') #감염자 동향 - 백분율
+        plt.plot(t, result[:,2]/N*100, 'g', label='Recovered') #회복자 동향 - 백분율
+        plt.plot(t, result[:,3]/N*100, 'y', label='Exposed') #잠복자 동향 - 백분율
         plt.xlabel('days')
-        plt.ylabel('number of people')
+        plt.ylabel('number of people (%)')
         plt.title('Republic of Korea')
         plt.legend(loc="upper right")
 
@@ -60,20 +66,19 @@ class get_KOR_Data():
         self.confirmed_data_POP = confirmed_data_POP
 
     def getIR(self, soup):
-        for pre_data_1 in soup.find_all('ul', class_='liveNum'):
-            confirmed_data_IR = pre_data_1.find_all('span', class_='num')
+        for pre_data_1 in soup.find_all('table', class_='num minisize'):
+            confirmed_data_IR = pre_data_1.find_all('td')
             for i in range(0, len(confirmed_data_IR)):
                 confirmed_data_IR[i] = confirmed_data_IR[i].get_text()
-        a = confirmed_data_IR[0]
-        confirmed_data_IR[0] = a[4:]
         return confirmed_data_IR
     
     def getPOP(self, soup):
-        for pre_data_2 in soup.find_all('article', class_='result'):
-            confirmed_data_POP = pre_data_2.find_all('h1')
+        global confirmed_data_POP
+        for pre_data_2 in soup.find_all('article', class_='intro'):
+            confirmed_data_POP = pre_data_2.find_all('b')
         confirmed_data_POP[0] = confirmed_data_POP[0].get_text()
-        a = confirmed_data_POP[0]
-        confirmed_data_POP[0] = a[:10]
+        #a = confirmed_data_POP[0]
+        #confirmed_data_POP[0] = a[:10]
         return confirmed_data_POP
     
     def convertToInt(self, _list):
@@ -113,7 +118,7 @@ class MyWindow(QWidget, Graph):
         self.initUI()
 
     def initUI(self):
-        self.fig = plt.figure(figsize=(10,10)) #출력되는 그래프 크기
+        self.fig = plt.figure(figsize=(70,70)) #출력되는 그래프 크기
         self.canvas = FigureCanvas(self.fig)
         self.toolbar = NavigationToolbar(self.canvas, self)
 
@@ -125,7 +130,7 @@ class MyWindow(QWidget, Graph):
 
         self.setLayout(layout)
 
-        self.setGeometry(300, 300, 300, 300)
+        self.setGeometry(300, 300, 700, 500)
         self.setWindowTitle("COVID-19 SIR forecasting model")
 
         self.show()
@@ -134,24 +139,28 @@ printGraph = Graph()
 GKD = get_KOR_Data(list(), list())
 
 pre_POP_KOR = GKD.convertToInt(GKD.getPOP(POPsoup_KOR)) #인구
-pre_IR_KOR = GKD.convertToInt(GKD.getIR(SIRsoup_KOR)) #(순서대로) - [0]누적 확진자수, [1]회복자수, [2]격리자수(치료자수), [3]사망자수
+pre_IR_KOR = GKD.convertToInt(GKD.getIR(SIRsoup_KOR)) #(순서대로) - [0]격리중, [1]격리해제, [2]사망, [3][0,1,2]합계, [4](검사)결과음성, [5][3,4]합계, [6]검사중, [7]총합계
 
 # <quit all drivers>
-SIRdriver_KOR.quit() 
+SIRdriver_KOR.quit()
 POPdriver_KOR.quit()
 
 # <constant>
-covid_19_R0 = 5.35 #코로나 바이러스 기초감염재생산수 = 5 ~ 5.7 이지만 임의로 3으로 지정함
-covid_19_R0_deviation = np.arange(5, 5.71, 0.01) #코로나 바이러스 기초감염재생산수 편차 저장용 리스트
-theta_input = 1/14 #코로나 바이러스 잠복기간 = 약 14일 이내 --> 회복률은 잠복기간의 역수인 1/14
+covid_19_R0 = 2.25 #코로나 바이러스 기초감염재생산수
+gamma_input = 1/14 #코로나 바이러스 전염가능시간 = 약 14일 이내 --> 회복률은 전염가능시간의 역수
+sigma_input = 1/14 #코로나 바이러스 평균잠복기간 = 약 14일 --> 잠복률은 평균잠복기간의 역수
 
 # <variable - korea>
 Population_input_KOR = pre_POP_KOR[0] #모집단 인구
-beta_input_KOR = theta_input*covid_19_R0/(Population_input_KOR - pre_IR_KOR[0]) # beta = theta*R0/S
-t_input_KOR = np.linspace(0, 100) #시간 범위 설정(가로축 범위 설정)
-x_input_KOR = [Population_input_KOR - pre_IR_KOR[0], pre_IR_KOR[2], pre_IR_KOR[1]] # 차례대로 S(취약자수), I(감염자수), R(회복자수)
+Susceptible = Population_input_KOR - pre_IR_KOR[0] - pre_IR_KOR[1] - pre_IR_KOR[6]
+Infected = pre_IR_KOR[0]
+Recovered = pre_IR_KOR[1]
+Exposed = pre_IR_KOR[6]
+beta_input_KOR = gamma_input*covid_19_R0/Susceptible # beta = gamma*R0/S
+t_input_KOR = np.linspace(0, 500) #시간 범위 설정(가로축 범위 설정) - 단위: 일(day)
+x_input_KOR = [Susceptible, Infected, Recovered, Exposed]
 
 app=QApplication(sys.argv)
 w=MyWindow()
-printGraph._plot(printGraph.SIR, x_input_KOR, t_input_KOR, beta_input_KOR, theta_input)
+printGraph._plot(printGraph.SEIR, x_input_KOR, t_input_KOR, beta_input_KOR, gamma_input, sigma_input, Population_input_KOR)
 sys.exit(app.exec_()) # exec_()가 루프를 생성하는 함수
